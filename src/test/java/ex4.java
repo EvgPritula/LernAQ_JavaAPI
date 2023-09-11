e06ff55bb7da825d286c04e8e69c4aaf122b4244
EPritula <eipritula@yandex.ru>
2023-09-11 21:48:06 +0300

    test commit2

---
+++ src/test/java/ex4.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+   public class ex4 {
+        @Test
+       public void testExample() {
+            Response response = RestAssured
+                    .get("https://playground.learnqa.ru/api/get_text")
+                    .andReturn();
+            response.prettyPrint();
+        }
+   }
+
