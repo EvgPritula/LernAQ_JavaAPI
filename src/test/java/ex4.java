78c1d88bbfb24a8bd57a1eea10a6b4f25aacfee6
EPritula <eipritula@yandex.ru>
2023-09-12 17:45:31 +0300

    test2

--- src/test/java/ex4.java
+++ src/test/java/ex4.java
@@ -1,13 +1,14 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-   public class ex4 {
-        @Test
-       public void testExample() {
-            Response response = RestAssured
-                    .get("https://playground.learnqa.ru/api/get_text")
-                    .andReturn();
-            response.prettyPrint();
-        }
-   }
 
+class Ex4 {
+
+    @Test
+    void testExample() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
