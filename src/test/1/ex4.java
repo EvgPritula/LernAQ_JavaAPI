e67299a525a304adee8d259f2bb743c652eb8247
EPritula <eipritula@yandex.ru>
2023-09-15 18:02:46 +0300

    test commit ex5

--- src/test/1/ex4.java
+++ src/test/1/ex4.java
@@ -2,7 +2,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-class Ex4 {
+public class Ex4 {
 
     @Test
     void testExample() {
