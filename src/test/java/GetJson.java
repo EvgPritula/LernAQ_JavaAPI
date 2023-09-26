a634c05a25d800749ffc9cb187b0d212970072a5
EvgPritula <49553393+EvgPritula@users.noreply.github.com>
2023-09-26 20:10:34 +0300

    Update and rename GetJson_Ex5.java to GetJson.java

--- src/test/java/GetJson.java
+++ src/test/java/GetJson.java
@@ -2,10 +2,10 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
-public class GetJsonEx5 {
+public class GetJson {
 
     @Test
-    public void GetJsonEx5() {
+    public void GetJson() {
         JsonPath response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
